f718afde3be1c52263a9463de020d15892d8087a
kyr <whyyouareay@gmail.com>
2023-05-05 18:41:44 +0900

    [feat] update github action for github builder

--- scrap/main.py
+++ scrap/main.py
@@ -1,3 +1,14 @@
 import os
+from github import Github
 
-print(os.environ["KYR_ACCESS_TOKEN"])
+# First create a Github instance:
+
+# using an access token
+g = Github(os.environ["KYR_ACCESS_TOKEN"])
+
+# # Github Enterprise with custom hostname
+# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
