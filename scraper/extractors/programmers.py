4ce9580652a73d285c3df033bbaa40054e19da8c
llattely2@gmail.com <llattely2@gmail.com>
2023-05-19 01:42:10 +0900

    [feat] update page

--- scraper/extractors/programmers.py
+++ scraper/extractors/programmers.py
@@ -157,10 +157,10 @@ def save_to_file_programmers(file_name, problems):
     # file = open(f'view/{file_name}.json', 'w', encoding='utf-8')
     file = open(f'vue-project/src/data/{file_name}.json', 'w', encoding='utf-8')
     file.write("programmers_problems = ")
-    data = {}
-    for i in range(0, len(problems)):
-        data[str(i+1)] = problems[i]
-    json.dump(data, file)
+    # data = {}
+    # for i in range(0, len(problems)):
+    #     data[str(i+1)] = problems[i]
+    json.dump(problems, file)
     file.close()
 
 
--- vue-project/src/components/MainPage.vue
+++ vue-project/src/components/MainPage.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <h1>[MainPage.vue]</h1>
+        {{ p_data }}
     </div>
 </template>
 
-<script setup>
-
+<script>
+// import p_data from "../data/programmers.json"
+export default {
+    setup() {
+    },
+    data() {
+        return {}
+    },
+}
 </script>
 
 <style>
