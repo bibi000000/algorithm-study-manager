376c2cae62d0a23bb7c2a1938dafad32e6f9430c
llattely2@gmail.com <llattely2@gmail.com>
2023-05-20 00:07:12 +0900

    [feat] update page

---
+++ asm/vue.config.js
@@ -0,0 +1,6 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/algorithm-study-manager/',
+  outputDir: 'dist'
+})
