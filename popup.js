4b663b926ec3d991bb318f22c15b3899cb382b01
llattely2@gmail.com <llattely2@gmail.com>
2023-05-23 17:46:05 +0900

    [feat] update pages

--- popup.js
+++
@@ -1,7 +0,0 @@
-const loadBtn = document.getElementById("load");
-loadBtn.addEventListener("click", () => {
-    alert("clicked")
-    chrome.runtime.sendMessage('get-total-programmers-challenges', (response) => {
-        console.log('received total programmers challenges data', response);
-    })
-})
--- scripts/content.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.sendMessage('get-total-programmers-challenges', (response) => {
-    console.log('received total programmers challenges data', response);
-    initializeUI(response);
-})
