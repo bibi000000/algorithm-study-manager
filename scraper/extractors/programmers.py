73158564a104d586e5ae982519f43f3e76519cf4
llattely2@gmail.com <llattely2@gmail.com>
2023-05-24 18:36:28 +0900

    [feat] update json

--- scraper/extractors/programmers.py
+++ scraper/extractors/programmers.py
@@ -85,7 +85,7 @@ def extract_programmers_challenges_solved():
             acceptance_rate = challenges[j].find_element(By.CLASS_NAME, 'acceptance-rate')
             solved = challenges[j].find_element(By.CLASS_NAME, 'status')
             link = title.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            key = link.spilit("/")[-1]
+            key = link.split("/")[-1]
             challenges_problems[key] = {
                 'title': title.find_element(By.TAG_NAME, 'a').text,
                 'part': title.find_element(By.CLASS_NAME, 'part-title').text,
@@ -143,7 +143,7 @@ def extract_programmers_challenges_total():
             acceptance_rate = challenges[j].find_element(By.CLASS_NAME, 'acceptance-rate')
             solved = challenges[j].find_element(By.CLASS_NAME, 'status')
             link = title.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            key = link.spilit("/")[-1]
+            key = link.split("/")[-1]
             challenges_problems[key] = {
                 'title': title.find_element(By.TAG_NAME, 'a').text,
                 'part': title.find_element(By.CLASS_NAME, 'part-title').text,
