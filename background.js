d33aee74f75a511fb6b61ab3a394f30761e9b05a
김민철 <kmc0968@naver.com>
2023-06-08 12:55:10 +0900

    modify

--- background.js
+++ background.js
@@ -1,63 +1,104 @@
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.type === 'githubLogin') {
-      if (request.state === 'start') {
-        console.log(11111111);
-        // GitHub OAuth 인증 URL을 생성합니다.
-        const clientId = "b59f5ff212158998d4a2";
-        const redirectUri = chrome.identity.getRedirectURL();
-        const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
-        console.log(redirectUri);
-        // 상태를 'redirect'로 저장
-        chrome.storage.local.set({ state: 'redirect' });
-  
-        // 새로운 탭에서 GitHub 로그인 페이지  열기
-        chrome.tabs.create({ url: authUrl });
-      } else if (request.state === 'redirect') {
-        console.log("12312312312312");
-        // redirect 단계에서 GitHub OAuth 코드
-        const code = request.code;
-        const clientId = "b59f5ff212158998d4a2";
-        const clientSecret = "4ecfeeb6d09638a55a44e123b91b4f60560a8d3b";
-        const redirectUri = chrome.identity.getRedirectURL();
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  if (request && request.closeWebPage === true && request.isSuccess === true) {
+   console.log("성공");
+  } else if (request && request.closeWebPage === true && request.isSuccess === false) {
+    alert('Something went wrong while trying to authenticate your profile!');
+    // chrome.tabs.getSelected(null, function (tab) {
+    //   chrome.tabs.remove(tab.id);
+    // });
+  } else{
+    console.log(" 다른거");
+  }
+  return true;
+});
+
+
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   console.log(sender + " " + sendResponse + " " + request);
+//   const st = '';
+//   if (request.type === 'githubLogin') {
+//     if (request.state === 'start') {
+//       // 로그인 시작 단계에서 GitHub OAuth 인증 URL을 생성합니다.
+//       const clientId = "b59f5ff212158998d4a2";
+//       const redirectUri = "https://lpgfgjpiojnpdiloofmicomgojgikmec.chromiumapp.org/";
+//       const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
+
+//       // 상태를 'redirect'로 저장합니다.
+//       chrome.storage.local.set({ state: 'redirect' }, function() {
+//         console.log(request.state + "12라인 "); // 'redirect' 출력
+//         console.log(chrome.storage.local);
         
-        // OAuth 코드를 사용하여 액세스 토큰을 요청합니다.
-        fetch('https://github.com/login/oauth/access_token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-          },
-          body: JSON.stringify({
-            client_id: clientId,
-            client_secret: clientSecret,
-            code: code,
-            redirect_uri: redirectUri
-          })
-        })
-        .then(response => response.json())
-        .then(data => {
-          const accessToken = data.access_token;
-          console.log("!23123123");
-  
-         
-  
-          chrome.storage.local.set({ state: 'done' });
-          console.log(data);
-          sendResponse({ success: true });
-        })
-        .catch(error => {
-          console.error(error);
-          sendResponse({ success: false });
-        });
+//       });
+
+//       // 새로운 탭에서 GitHub 로그인 페이지를 엽니다.
+//       chrome.tabs.create({ url: authUrl }, function(tab) {
+//         const tabId = tab.id;
+//         console.log(tab );
         
-      }
-      else {
-        // 올바르지 않은 상태 값일 경우 처리
-        console.error('Invalid state value');
-        sendResponse({ success: false });
-      }
-      
-      return true; 
-    }
-  });
-  
+//         // 탭이 완전히 로드되었을 때 실행되는 콜백 함수
+//         chrome.tabs.onUpdated.addListener(function(updatedTabId, changeInfo, updatedTab) {
+//           if (updatedTabId === tabId && changeInfo.status === 'complete') {
+//             // 탭이 완전히 로드된 후에 탭 상태를 확인합니다.
+//             chrome.tabs.get(tabId, function(updatedTab) {
+//               console.log(updatedTab.status + " 24라인"); // 'complete' 출력
+//               // console.log(response);
+//               chrome.storage.local.set({state : 'redirect'});
+//               console.log(chrome.storage.local.get('state'));
+              
+//               console.log("11111");
+//               sendResponse({state : "redirect"});
+//               window.close();
+//               // 필요한 작업을 수행합니다.
+
+//               // 이후의 코드를 계속 작성합니다.
+//             });
+//           }
+//         });
+//       });
+     
+//     } else if (request.state === 'redirect') {
+//       console.log("Redirect");
+//       // 로그인 리디렉션 단계에서 GitHub OAuth 코드를 받아옵니다.
+//       const code = request.code;
+//       const clientId = "b59f5ff212158998d4a2";
+//       const clientSecret = "4ecfeeb6d09638a55a44e123b91b4f60560a8d3b";
+//       const redirectUri = "https://hlcknjlhodaiinfhbcppnlpkmidfmkal.chromiumapp.org/";
+
+//       // OAuth 코드를 사용하여 액세스 토큰을 요청합니다.
+//       fetch('https://github.com/login/oauth/access_token', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Accept: 'application/json'
+//         },
+//         body: JSON.stringify({
+//           client_id: clientId,
+//           client_secret: clientSecret,
+//           code: code,
+//           redirect_uri: redirectUri
+//         })
+//       })
+//       .then(response => response.json())
+//       .then(data => {
+//         const accessToken = data.access_token;
+
+//         // 액세스 토큰을 사용하여 GitHub API에 요청을 보낼 수 있습니다.
+//         // 이 부분에서 원하는 작업을 수행할 수 있습니다.
+
+//         chrome.storage.local.set({ state: 'done' });
+//         sendResponse({ success: true });
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         sendResponse({ success: false });
+//       });
+//     } else {
+//       console.log('Invalid state value:', request.state);
+//       // 올바르지 않은 상태 값일 경우 처리
+//       sendResponse({ success: false });
+//     }
+
+//     return true; // 비동기 응답을 위해 true를 반환합니다.
+//   }
+// });
--- popup.js
+++ popup.js
@@ -1,17 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+let action = false;
+
+// document.addEventListener('DOMContentLoaded', function() {
   const loginButton = document.getElementById('loginButton');
   loginButton.addEventListener('click', function() {
-    chrome.runtime.sendMessage({ type: 'githubLogin', state: 'start' }, function(response) {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
-
-      if (response.success) {
-        console.log('로그인 성공');
-      } else {
-        console.error('로그인 실패.');
-      }
-    });
+    oAuth2.start();
   });
-});
+// });
+
